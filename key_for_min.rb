ecf56e61ef7bd5e77a4e1842dcd07e85b1e9adef
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-05 17:44:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,14 @@
 
 def key_for_min_value(name_hash)
   num = 0
-  name_hash.map do |value, key|
+  name_hash.each do |value, key|
     if name_hash[value] < num
       num = key
     end
+  end
+
+  name_hash.map do |value, key|
     if name_hash[value] == num
       value
-    end
-  end
+    end 
 end
