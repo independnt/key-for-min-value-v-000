fc851d190555587d0eae28e51c2755218dfba33b
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-05 17:57:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,13 @@ require "pry"# Given a hash with numeric values, return the key for the smallest
 def key_for_min_value(name_hash)
   num = 1000
   name_hash.each do |value, key|
-    puts value[key]
-    binding.pry
+    if key > num
+      num = key
+    end
   end
 
   name_hash.map do |value, key|
-    if name_hash[value] == num
+    if key == num
       value
     end
   end
