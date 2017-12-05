36c7f957283c856b0deab09396705ea0ca96e483
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-05 18:01:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
 require "pry"# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  num = 1000
   name_hash.each do |value, key|
-    num = 1000
     if key < num
       num = key
     end
